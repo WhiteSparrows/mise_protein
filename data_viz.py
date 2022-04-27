678c8d757b8c5f1c8acba4d6313ab5e3e56ba4b5
Nicolas Cavaillès--Wurmser <nicolas.cavailles--wurmser@grenoble-inp.org>
2022-04-27 20:05:56 +0200

    pca done

--- data_viz.py
+++ data_viz.py
@@ -8,12 +8,12 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder
 from preprocessing import imputation, encoding
+from sklearn.pipeline import make_pipeline
 
 df = pd.read_csv('Data_Cortex_Nuclear.csv')
 df_save = df.copy()
 
 print(df.head())
-df_vals = df.drop(['class'],axis=1).values
 labels = df['class'].values # labels
 
 
@@ -25,61 +25,62 @@ plt.title("Class Distribution")
 plt.xticks(range(len(LABELS_unique)), LABELS_unique)
 plt.xlabel("Class")
 plt.ylabel("Frequency")
+print('Repartition of classes')
 print(df['class'].value_counts(normalize=True))
 
-
-#encoding of the class
-label_encoder = LabelEncoder()
-for col in ['Genotype', 'Treatment', 'Behavior', 'class']:
-    df[col] = label_encoder.fit_transform(df[col])
-X = df_vals
-#X = StandardScaler().fit_transform(df_vals)
-
-print(df.head())
+for col in df.select_dtypes("object"):
+    plt.figure()
+    df[col].value_counts().plot.pie()
 
 
-
-
-"""
-Data viz
-"""
 #heatmap of na
 plt.figure(figsize=(10,10))
 ax = plt.axes()
 sns.heatmap(df.isna(),cbar=False)
 ax.set_title('Heat map of NA')
 
+#####" PREPROCESSING ##########""
+#encoding of the class
+label_encoder = LabelEncoder()
+for col in ['Genotype', 'Treatment', 'Behavior', 'class']:
+    df[col] = label_encoder.fit_transform(df[col])
+df = df.drop('MouseID', axis=1)
+#df = df.dropna(axis=0)
+df = df.fillna(df.mean())
+#X = StandardScaler().fit_transform(df_vals)
+
+X = df.drop('class', axis=1)
+y = df['class'].astype(int)
+
+
+print(df.head())
+
+X_train, X_test,y_train, y_test = train_test_split(X, y, test_size = 0.3)
+
+
+"""
+Data viz
+"""
+
 #  Bar plot
 
 plt.figure(figsize=(10,10))
 ax = plt.axes()
-sns.heatmap(data=df.corr(),  annot = True) #arg for color is cmap with for example cmap = 'seismic'
-ax.set_title('Heat map')
+sns.heatmap(data=df.corr(),  annot = False,cmap="YlGnBu", vmin=-1, vmax=+1) #arg for color is cmap with for example cmap = 'seismic'
+ax.set_title('Correlation heat map')
+
+#PCA
+preprocessor = make_pipeline(StandardScaler())
+
+PCAPipeline = make_pipeline(preprocessor, PCA(n_components=2,random_state=0))
 
 
-"""
-pca = PCA(n_components=2)
-principalComponents = pca.fit_transform(X)
-principalDf = pd.DataFrame(data = principalComponents, 
-                           columns = ['principal component 1', 'principal component 2'])
-finalDf = pd.concat([principalDf, df['class']], axis = 1)
-#print(finalDf.head())
-fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(1,1,1)
-ax.set_xlabel('Principal Component 1', fontsize = 20)
-ax.set_ylabel('Principal Component 2', fontsize = 20)
-ax.set_title('2 components PCA', fontsize = 20)
-targets = [0, 1]
-colors = ['r', 'g']
-for target, color in zip(targets,colors):
-    indicesToKeep = finalDf['class'] == target
-    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-               , finalDf.loc[indicesToKeep, 'principal component 2']
-               , c = color
-               , s = 50)
-ax.legend(targets)
-ax.grid()
 
-"""
+PCA_df = pd.DataFrame(PCAPipeline.fit_transform(X))
+PCA_df = pd.concat([PCA_df, df['class']], axis=1)
 
+
+plt.figure(figsize=(8,8))
+sns.scatterplot(PCA_df[0],PCA_df[1],hue=PCA_df['class'],palette=sns.color_palette("Paired", 8))
+plt.title('PCA')
 plt.show()
--- main.py
+++ main.py
@@ -6,6 +6,104 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+def score_classifier(dataset,classifier,labels):
+
+    """
+    performs 3 random trainings/tests to build a confusion matrix and prints results with precision and recall scores
+    :param dataset: the dataset to work on
+    :param classifier: the classifier to use
+    :param labels: the labels used for training and validation
+    :return:
+    """
+
+    kf = KFold(n_splits=5,random_state=50,shuffle=True)
+    confusion_mat = np.zeros((2,2))
+    recall = 0
+    prec = 0
+    f1 = 0
+    acc = 0
+    auc = 0
+    fpr = []
+    tpr = []
+    tprs = []
+    aucs = []
+    fig, ax = plt.subplots()
+    mean_fpr = np.linspace(0, 1, 100)
+    for i, (training_ids,test_ids) in enumerate(kf.split(dataset)):
+        training_set = dataset[training_ids]
+        training_labels = labels[training_ids]
+        test_set = dataset[test_ids]
+        test_labels = labels[test_ids]
+        classifier.fit(training_set,training_labels)
+        predicted_labels = classifier.predict(test_set)
+        viz = RocCurveDisplay.from_estimator(
+            classifier,
+            test_set,
+            test_labels,
+            name=f"ROC fold {i} for model {classifier}",
+            alpha=0.3,
+            lw=1
+        )
+        plt.close()
+        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(viz.roc_auc)
+        mean_tpr = np.mean(tprs, axis=0)
+        mean_tpr[-1] = 1.0
+        mean_auc = metrics.auc(mean_fpr, mean_tpr)
+        std_auc = np.std(aucs)
+        ax.plot(
+            mean_fpr,
+            mean_tpr,
+            color="b",
+            label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
+            lw=2,
+            alpha=0.8,
+        )
+
+        std_tpr = np.std(tprs, axis=0)
+        tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+        tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+        ax.fill_between(
+            mean_fpr,
+            tprs_lower,
+            tprs_upper,
+            color="grey",
+            alpha=0.2,
+            label=r"$\pm$ 1 std. dev.",
+        )
+
+        ax.set(
+            xlim=[-0.05, 1.05],
+            ylim=[-0.05, 1.05],
+            title="Receiver operating characteristic example",
+        )
+        ax.legend(loc="lower right")
+        confusion_mat+=confusion_matrix(test_labels,predicted_labels)
+        recall += recall_score(test_labels, predicted_labels)
+        prec += precision_score(test_labels, predicted_labels)
+        f1 += f1_score(test_labels, predicted_labels)
+        acc += accuracy_score(test_labels, predicted_labels)
+        y_pred_proba = classifier.predict_proba(test_set)[::,1]
+        fpr, tpr, _ = metrics.roc_curve(test_labels, y_pred_proba)
+        auc += metrics.roc_auc_score(test_labels, y_pred_proba)
+    recall/=5
+    prec/=5
+    f1/=5
+    acc/=5
+    auc/=5
+    plt.close()
+    print(confusion_mat)
+    print(f"The accuracy is : {acc}")
+    print(f"The recall score is : {recall}")
+    return {'acc': acc, 'prec': prec, 'rec': recall, 'f1': f1,  
+            'fpr': mean_fpr, 'tpr': mean_tpr, 'auc': auc, 'cm': confusion_mat}
+
+
+
+
 df = pd.read_csv('Data_Cortex_Nuclear.csv')
 df_save = df.copy()
 print(df.head())
